ab7e1c84cd839305928aa011fa7760e821be85d2
rthugh02 <hughes.read@gmail.com>
2020-02-24 12:28:08 -0500

    create dense layer

--- src/DenseNetwork.h
+++ src/DenseNetwork.h
@@ -1,13 +1,24 @@
 #ifndef DENSENETWORK_H
 #define DENSENETWORK_H
-
+#include <armadillo>
 class DenseNetwork
 {
 private:
-    /* data */
+    //dense layer weights
+    arma::mat weights1;
+    arma::mat weights2;
+    arma::mat weights3;
+
+    //batch data
+    arma::mat data;
+
+    void flatten_data(arma::cube * data);
 public:
-    DenseNetwork();
+    DenseNetwork(arma::cube * data);
     ~DenseNetwork();
+
+    arma::mat calculate_output(void (*activation_func)(arma::mat *, const char *));
+    void set_data(arma::cube * data);
 };
 
 
